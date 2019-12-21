279b61813ae0386f8ad73171035ce3403f8ce739
Brandon Solis <bsolis@codeaurora.org>
2019-12-21 12:56:33 +0100

    X00P: Update user for ims daemons
    Change user for imsdatadaemon, imsrcsd,
    ims_rtp_daemon
    from AID_SYSTEM to AID_RADIO
    
    Change-Id: I8ee1c4ff182af0d9b4ebf028e3096cfa88118287
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.fs
+++ config.fs
@@ -25,6 +25,12 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+[vendor/bin/pd-mapper]
+mode: 0755
+user: AID_RADIO
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
 [vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_RADIO
