ab7891d7f2c4f071aab0d43fc0dabeb3707c3198
nysascape <nysadev@raphielgang.org>
2020-01-28 10:54:25 +0000

    X00P: treewide: power: Switch to 1.3 libperfmanager from Wahoo
    * Use my HAL with dt2w from daisy, initially from 660
    * Custom powerhint with values set for Daisy (thanks uvera for the base config)
    * Based on: https://github.com/sdm660/device_xiaomi_jason/commit/fa96d448b214354b15833bcba74b757c8c123f09#diff-3b27725fa91399ccbb4e4e7a2f8ccff8
    
    Huge thanks to uvera and celtare21 <3
    
    Signed-off-by: nysascape <nysadev@raphielgang.org>
    Change-Id: I276170dc223c19afc42f5f22660081f797e0b864

--- device.mk
+++ device.mk
@@ -279,11 +279,11 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power@1.3-service.pixel-libperfmgr \
+    android.hardware.power.stats@1.0-service.pixel
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QMI
 PRODUCT_PACKAGES += \
