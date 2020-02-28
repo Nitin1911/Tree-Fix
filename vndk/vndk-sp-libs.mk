4d6d6fee2a83c1c898c6bb11f70ca108a1e9cc50
ZeeLog <Danjel.Zammit@gmail.com>
2020-02-28 09:58:29 +0100

    X00P: Build display config (1-9) extra vendor libs
    Change-Id: Id3092139212732c572670460bb8d5e5461163533

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -38,4 +38,14 @@ VNDK_SP_LIBRARIES := \
     libz
 
 EXTRA_VENDOR_LIBRARIES := \
-    vendor.qti.hardware.cryptfshw@1.0
+    vendor.qti.hardware.cryptfshw@1.0 \
+    vendor.display.config@1.0 \
+    vendor.display.config@1.1 \
+    vendor.display.config@1.2 \
+    vendor.display.config@1.3 \
+    vendor.display.config@1.4 \
+    vendor.display.config@1.5 \
+    vendor.display.config@1.6 \
+    vendor.display.config@1.7 \
+    vendor.display.config@1.8 \
+    vendor.display.config@1.9
