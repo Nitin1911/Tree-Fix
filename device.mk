a7acf11116888a0f7fc46f8f4a0d0cd4dcd5c4a4
MD Raza <raza231198@gmail.com>
2020-03-31 16:56:46 +0530

    X00P: import Sensors Config
    * fix derps
    * from asus/WW_X00PD/ASUS_X00P_4:9/PPR1.180610.009/16.00.1906.411:user/release-keys
    
    Signed-off-by: MD Raza <raza231198@gmail.com>
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>

--- device.mk
+++ device.mk
@@ -20,10 +20,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay
-
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
