7db3b10b3182cc355b5c115d6d374ccd47b732ef
iamsaalim <saalimquadri1@gmail.com>
2020-04-06 22:10:38 +0200

    X00P: rename some blobs to msm8937
    * fix derps
    * we build msm8937 blobs so custom vendor blobs are needed to be renamed to run
    
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>

--- device.mk
+++ device.mk
@@ -288,11 +288,6 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
 
-# QCOM
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
-
 # RCS
 PRODUCT_PACKAGES += \
     rcs_service_aidl \
